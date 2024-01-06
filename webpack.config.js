29f6774fa06392789bea7726610d5d45d260d425
HPatto <hpatto97@gmail.com>
2024-01-06 18:23:09 -0300

    Add basic webpack layout

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    devtool: 'inline-source-map',
+    module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      // Add more loaders for different file types if needed
+    ],
+  },
+  // Add plugins or other configurations as needed
+};
